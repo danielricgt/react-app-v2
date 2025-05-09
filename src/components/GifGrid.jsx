296b1068d939b3f32e0b7c41f69a66827c0ce990
Daniel galvan <danielricardogt@gmail.com>
2025-05-08 22:39:49 -0500

    :wrench: fetch api with functioal components

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { getGifs } from '../helpers/getgifs';
+import { useState } from 'react';
 
 
 
 export const GifGrid = ({ category }) => {
 
+    const [images, setImages] = useState([]);
 
+    const getImages = async () => {
+        const newImages = await getGifs(category);
+        setImages(newImages);
+    }
 
-    getGifs(category);
- 
+    useEffect( () => {
+        getImages();
+    }, [] );
+        
+  
     return (
         <>
             <h1>{category}</h1>
-            <h3>Hola Mundo</h3>
+            <ol>
+                <li>TITULO 1</li>
+                <li>titulo 2</li>
+                <li>titulo 3</li>
+                <li>titulo 4</li>
+            </ol>
         </>
     )
 }
