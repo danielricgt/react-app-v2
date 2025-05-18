9a224918252716536a9d220c0d97094a65dcb6ff
Daniel galvan <danielricardogt@gmail.com>
2025-05-17 22:13:38 -0500

    :wrench: api key .env

--- src/helpers/getgifs.js
+++ src/helpers/getgifs.js
@@ -1,5 +1,6 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=lYx36jjAEPoVYEvOzhHTfW5dtrEaAgEz&q=${category}&limit=20`;
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=20`;
     const resp = await fetch(url);
     const {data} = await resp.json();
     // console.log(data);
