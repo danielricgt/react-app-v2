9245399a6175329bde463fdc78a06fe445509d9e
Daniel galvan <danielricardogt@gmail.com>
2025-05-19 21:32:18 -0500

    :white_check_mark: testing part II

--- test/components/GitItem.test.js
+++ test/components/GitItem.test.js
@@ -1,29 +1,28 @@
-import { render } from "@testing-library/react";
+import { getByRole, render, screen } from "@testing-library/react";
 import { GitItem } from "../../src/components/GitItem";
 
+describe("GitItem component", () => {
+  const title = "Yes jest";
+  const url = "http://google.com.jpg/";
 
-describe('test on gitItem', () => { 
+  test("should match the snapshot", () => {
+    const { container } = render(<GitItem title={title} url={url} />);
+    expect(container).toMatchSnapshot();
+  });
 
-    test('must to do match with the snapshop', () => {
-        const title = 'Yes jest';
-        const url = 'http://google.com';
-        const {container} = render(<GitItem title={title} url={url}/>);
-        expect(container).toMatchSnapshot();
-    });
-    
-
- })
-
-//  export const GitItem = ({title, url}) => {
-
-//     //  console.log(title, url);
-//    return (
-//       <div className="card" >
- 
-//          <img src={url} alt={title} />
-//          <p>{title}</p>
- 
-//       </div>
-//    )
-//  }
- 
+  test("should show the image with the url and the alt indicated", () => {
+    //   first we take our test subject
+    render(<GitItem title={title} url={url} />);
+    // expect(screen.getByRole('img').src).toBe( url );
+    // expect(screen.getByRole('img').alt).toBe( title  );
+    const { src, alt } = screen.getByRole("img");
+    expect(src).toBe(src);
+    expect(alt).toBe(alt);
+  });
+  test('must show the title in the component', () => {
+        //   first we take our test subject
+        render(<GitItem title={title} url={url} />);
+        expect(screen.getAllByText(title)).toBeTruthy();
+  });
+  
+});
