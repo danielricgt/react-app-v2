8db3c793484d94f91be7d4f6a85ba0e54c7a2ead
Daniel galvan <danielricardogt@gmail.com>
2025-05-21 21:46:40 -0500

    :white_check_mark: testing part III

--- src/helpers/getgifs.js
+++ src/helpers/getgifs.js
@@ -1,17 +1,11 @@
-export const getGifs = async (category) => {
-    const apiKey = import.meta.env.VITE_API_KEY;
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=20`;
-    const resp = await fetch(url);
-    const {data} = await resp.json();
-    // console.log(data);
+export const getGifs = async (query, apiKey) => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(query)}&limit=10`;
+    const res = await fetch(url);
+    const { data } = await res.json();
 
-    const gifs = data.map(img => ({
+    return data.map(img => ({
         id: img.id,
         title: img.title,
-        url : img.images.downsized_medium.url
-
+        url: img.images?.downsized_medium?.url
     }));
-
-    return gifs;
-
-}
+};
